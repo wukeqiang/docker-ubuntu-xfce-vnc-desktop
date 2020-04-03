940c248a23c02f3ec4fa2814fbb90f0bf7139d3f
Chen Wang <mr_cwang@foxmail.com>
2020-04-03 23:28:24 +0800

    copy terminal to depsktop

--- startup.sh
+++ startup.sh
@@ -6,5 +6,10 @@ mkdir -p /var/run/sshd
 # start up supervisord, all daemons should launched by supervisord.
 /usr/bin/supervisord -c /root/supervisord.conf
 
+# copy terminal to depsktop
+mkdir -p /root/Desktop/
+cp -f /usr/share/applications/xfce4-terminal.desktop /root/Desktop/xfce4-terminal.desktop
+
 # start a shell
 /bin/bash
+
