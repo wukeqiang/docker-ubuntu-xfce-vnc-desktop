aea1ce339ff687aef5ae84a0585c628f1fffdf2d
Chen Wang <mr_cwang@foxmail.com>
2020-04-05 17:29:59 +0800

    fix terminal

--- startup.sh
+++ startup.sh
@@ -9,6 +9,11 @@ mkdir -p /var/run/sshd
 # copy terminal to depsktop
 mkdir -p /root/Desktop/
 cp -f /usr/share/applications/xfce4-terminal.desktop /root/Desktop/xfce4-terminal.desktop
+chmod +x /root/Desktop/xfce4-terminal.desktop
+echo 2 | update-alternatives --config x-terminal-emulator >> /dev/null 2>&1
+
+# print
+echo "Now you can use vnc viewer to connect the container ^_^"
 
 # start a shell
 /bin/bash
