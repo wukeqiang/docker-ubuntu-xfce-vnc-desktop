e17367eb912bf5379b4d17758996524ea0c5eb8a
Chen Wang <mr_cwang@foxmail.com>
2020-04-05 17:28:46 +0800

    add information

--- startup.sh
+++ startup.sh
@@ -12,5 +12,8 @@ cp -f /usr/share/applications/xfce4-terminal.desktop /root/Desktop/xfce4-termina
 chmod +x /root/Desktop/xfce4-terminal.desktop
 echo 2 | update-alternatives --config x-terminal-emulator >> /dev/null 2>&1
 
+# print
+echo "Now you can use vnc viewer to connect the container ^_^"
+
 # start a shell
 /bin/bash
